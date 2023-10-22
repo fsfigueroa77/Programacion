767230009a68b326b7b20ff2a047af3462e4fd34
fsfigueroa77 <fsfigueroa77@gmail.com>
2023-10-22 00:14:16 -0500

    Area y Volumen de una Esfera

---
+++ 3.Area_Volumen_Esfera/estructurada.c
@@ -0,0 +1,38 @@
+//1) Realizar un programa que muestre el area y volumen de una esfera considerando el radio ingresado por el usuario.//
+
+#include <stdio.h>
+#define pi 3.1415
+int radio;
+int leerradio();
+void calculararea(float radio);
+void calcularvolumen(float radio);
+
+int main()
+{
+    printf("Programa para calcular Area y Volumen de la esfera de radio ingresado por el usuario.\n");
+    int radio = leerradio();
+    calculararea((float)radio);
+    calcularvolumen((float)radio);
+
+    return(0);
+}
+
+int leerradio()
+{
+    int input_radio;
+    printf("Ingrese el valor del radio de la esfera: ");
+    scanf("%d", &input_radio);
+    return(input_radio);
+}
+
+void calculararea(float radio)
+{
+    float op_area = 4 * pi * radio * radio;
+    printf("El valor del area de la esfera es: %.2f\n", op_area);
+}
+
+void calcularvolumen(float radio)
+{
+    float op_volumen = (4 * pi * radio * radio * radio) / 3;
+    printf("El valor del volumen de la esfera es: %.2f", op_volumen);
+}
---
+++ 3.Area_Volumen_Esfera/secuencial.c
@@ -0,0 +1,19 @@
+//1) Realizar un programa que muestre el area y volumen de una esfera considerando el radio ingresado por el usuario.//
+
+#include <stdio.h>
+#define pi 3.1415
+
+int main()
+{
+    int radio;
+    
+    printf("Programa para calcular el area y volumen de una esfera.\nIngrese el valor del radio de la esfera: ");
+    scanf("%d", &radio);
+
+    float area = 4 * pi * (radio * radio);
+    float volumen = (4 * pi * (radio * radio * radio)) / 3;
+    
+    printf("El valor de area la esfera es: %.2f \nEl valor del volumen de la esfera es: %.2f", area, volumen);
+
+    return (0);
+}
